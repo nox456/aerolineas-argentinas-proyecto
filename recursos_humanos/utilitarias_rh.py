1e83cb46fc7b52d43bc3e84498c81fa693f1f879
Abisaac1809 <abisaaccarmona@gmail.com>
2024-11-25 13:44:23 -0400

    Se agrega funcion liquidacion, con motivos de renuncia y despido

--- recursos_humanos/utilitarias_rh.py
+++ recursos_humanos/utilitarias_rh.py
@@ -8,6 +8,13 @@ def diccionario():
     "agregarArchivo": agregarArchivo,
     "escribirArchivo": escribirArchivo,
     "entero": validarInt,
+    "validarOpcionDict": validarOpcionDict,
+    "validarDia": validarDia,
+    "validarMes": validarMes,
+    "validarAno": validarAno,
+    "crearArchivo": crearArchivo,
+    "validarNombre": validarNombre,
+    "manejoNombre" : manejoNombre
 }
 
 
@@ -37,7 +44,7 @@ def leerArchivo(ruta):  # object archivo
 def agregarArchivo(ruta):  # object archivo
     archivo = object
     try:
-        archivo = open(ruta, "a")
+        archivo = open(ruta, "ab")
         return archivo
     except FileNotFoundError:
         print("Objet-File: Archivo no encontrado. ")
@@ -68,6 +75,71 @@ def validarInt(n):
             print("ERROR: Valor incorrecto!")
             n = input("Ingrese el valor correcto: ")            
 
+
+def validarOpcionDict(opcion, diccionario):
+    while True:
+        try:
+            opcion = int(opcion)
+            if  1 <= opcion <= (len(diccionario) + 1):
+                return opcion
+            opcion = input("Ingrese una opción válida: ")
+        except ValueError:
+            print("Value Error: The data could not be converted to integer.")
+            opcion = input("Ingrese una opción válida: ")
+            
+            
+def validarDia(dia):
+    while True:
+        dia = validarInt(dia)
+        if 1 <= dia <= 31:
+            return dia 
+        dia = input("Ingrese un día válido: ")
+
+
+def validarMes(mes):
+    while True:
+        mes = validarInt(mes)
+        if 1 <= mes <= 12:
+            return mes 
+        mes = input("Ingrese un mes válido: ")
+        
+
+def validarAno(ano):
+    while True:
+        ano = validarInt(ano)
+        if ano >= 1950:
+            return ano 
+        ano = input("Ingrese un año válido: ")
+    
+    
+def crearArchivo(nombre): # archivo .txt
+    contador = 1
+    while True:
+        try:
+            archivo = object # objeto
+            archivo = open("{}.txt".format(nombre), "x")
+            return archivo
+        except FileExistsError:
+            nombre += "({})".format(contador)
+            contador += 1
+        
+
+
+def validarNombre(nombre):
+    tupla = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", " ","@", "#", "$",
+             "!", "%", "&", ".", ",", ":", ";", "*", "+", "-", "_", "¿", "?", "¡")
+    for i in range(len(nombre)):
+        for j in range(len(tupla)):
+            if nombre[i] == tupla [j]:
+                    return False
+    return True
+
+def manejoNombre(nombre):                    
+    while True:
+        if (validarNombre(nombre) == False):
+            nombre = input("Ingrese un nombre o apellido válido: ")
+        else:
+            return nombre
 
 
 validar = diccionario()
